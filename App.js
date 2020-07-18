9026d02920ca8d22cb60cd79202f88be3fc45d82
Alekhya <vinnakotaalekhya@gmail.com>
2020-07-18 00:06:07 -0500

    Redux store persist

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore'
+import { ConfigureStore } from './redux/configureStore';
+import { persistGate, PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
 
 //connecting redux store to the root component that renders the main component. This allows all the child components that are routed in main component to utilize the redux store
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
     return (
         <Provider store={store}>
+            <PersistGate
+            Loading={<Loading/>}
+            persistor={persistor}>
             <Main />
+            </PersistGate>
         </Provider>
     );
 } 
